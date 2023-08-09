5316eec16106de68b5082fb981cece7f23176aea
owizy <128070074+owizy@users.noreply.github.com>
2023-08-09 16:17:37 +0100

    Update setting.js

--- src/Component/Chatcard/Profile/setting.js
+++ src/Component/Chatcard/Profile/setting.js
@@ -20,7 +20,7 @@ import Switch from '@mui/material/Switch';
 import darkimg from "../../Screenshot (115).png"
 import { ThemeContext } from '../../../context/Themecontext';
 import { AuthContext } from '../../../context/AuthContext';
-
+import Avatar from '@mui/material/Avatar';
 
 const MaterialUISwitch = styled(Switch)(({ theme }) => ({
     width: 62,
@@ -68,7 +68,35 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
       borderRadius: 20 / 2,
     },
   }));
-  
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',  
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
 
 function Setting(){
       const navigate = useNavigate()
